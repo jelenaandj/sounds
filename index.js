c7724f07fd1c7c5fc7bf4fa434bf2fd64b9530fd
jelenaandj <jelena.andjelkovic453@gmail.com>
2020-06-20 00:27:09 +0200

    added blur

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const board=document.querySelector('.board');
-const pic=document.querySelector('img');
 // let cat=document.querySelector('.cat');
 const cards=board.children;
 
@@ -7,19 +6,21 @@ let card=[...cards]
 
 card.forEach((card,index)=>{
     card.addEventListener('click',(e)=>{
+        const pic=document.querySelector(`img[index='${index}']`);
         const audio=document.querySelector(`audio[index='${index}']`);
         audio.play();
         console.log(card.className);
         ///create label
         let title=document.createElement('label');
         title.className='title';
-        title.innerText=card.className;
-        card.appendChild(title)
+        title.innerText=card.className.toUpperCase();
+        card.appendChild(title);
+        pic.style='filter:blur(5px)';
         console.log('title',title);
-        pic.blur;
 
         setTimeout(()=>{
             title.remove();
+            pic.style='none';
         },2000)
     });
 });
